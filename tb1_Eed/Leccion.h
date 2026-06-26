1b6ba140959339178e06a20629d0d4de92242479
Kassandra <kassandra26a@gmail.com>
2026-06-26 12:20:28 -0500

    Eliminación de la clase ejercicio

--- tb1_Eed/Leccion.h
+++ tb1_Eed/Leccion.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include "Ejercicio.h"
 #include "Lista.hpp"
 #include "Cola.hpp" 
 #include "Progreso.h"
@@ -13,7 +12,6 @@ class Leccion {
 public:
     virtual ~Leccion();
 
-    // Métodos comunes que cada idioma debe implementar
     virtual void ordenarOracion(Progreso& progreso) = 0;
     virtual void completarOracion(Progreso& progreso) = 0;
     virtual void traduccionAvanzada(Progreso& progreso) = 0;
